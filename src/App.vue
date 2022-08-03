98fae139def058c03ae8d39f8883e8ff67c98cbf
antonio <antoniogomesdev@gmail.com>
2022-08-03 12:38:02 -0300

    esqueleto inicial

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+
+const drawer = ref(true);
+</script>
+<template>
+  <v-app>
+    <v-app-bar color="primary">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </template>
+      <v-app-bar-title>2BDone</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" left>
+      <v-row no-gutters>
+        <p>alo</p>
+      </v-row>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style>
+.v-application {
+  font-family: "Times New Roman";
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+<template>
+  <div class="pa-10">
+    <v-row justify="space-between" dense>
+      <p class="text-h4">Quadro</p>
+      <v-btn flat icon="mdi-dots-vertical"></v-btn>
+    </v-row>
+    <v-row dense class="mt-15">
+      <v-card min-height="300" min-width="300">
+        <v-card-title>
+          <v-row justify="space-between">
+            <div class="d-flex">
+              <p>To do</p>
+              <p class="ml-3">3</p>
+            </div>
+            <v-btn flat icon="mdi-dots-vertical"></v-btn>
+          </v-row>
+        </v-card-title>
+        <v-card-text>
+          <v-sheet>
+            <v-row>
+              <v-col cols="2"> a </v-col>
+              <v-col cols="10"> b </v-col>
+            </v-row>
+          </v-sheet>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </div>
+</template>
