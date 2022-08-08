bfc9ce02fc710d69e133dd966ed4bfa3dbe9ccca
antonio <antoniogomesdev@gmail.com>
2022-08-08 13:47:39 -0300

    changed vuex to pinia

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,31 @@
-import { createStore } from "vuex";
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+export const useStore = defineStore("items", () => {
+  const boards = ref([
+    {
+      name: "Quadro Teste",
+      sections: [
+        {
+          name: "To Do",
+          items: [
+            {
+              done: false,
+              description: "Alooou!",
+            },
+          ],
+        },
+        {
+          name: "Doing",
+          items: [
+            {
+              done: false,
+              description: "hey!",
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+  return { boards };
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,58 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { useStore } from "@/store/index";
+
+const store = useStore();
+
+const toggle = ref(false);
+</script>
 <template>
   <div class="pa-10">
     <v-row justify="space-between" dense>
-      <p class="text-h4">Quadro</p>
+      <p class="text-h4">{{ store.boards[0].name }}</p>
       <v-btn flat icon="mdi-dots-vertical"></v-btn>
     </v-row>
     <v-row dense class="mt-15">
-      <v-card min-height="300" min-width="300">
+      <v-card
+        flat
+        height="300"
+        width="300"
+        v-for="section in store.boards[0].sections"
+        :key="section.name"
+      >
         <v-card-title>
           <v-row justify="space-between">
             <div class="d-flex">
-              <p>To do</p>
-              <p class="ml-3">3</p>
+              <p>{{ section.name }}</p>
+              <p class="ml-3">{{ section.items.length }}</p>
             </div>
             <v-btn flat icon="mdi-dots-vertical"></v-btn>
           </v-row>
         </v-card-title>
         <v-card-text>
-          <v-sheet>
+          <v-sheet
+            v-for="item in section.items"
+            :key="item.description"
+            rounded
+            style="border: 1px solid black"
+          >
             <v-row>
-              <v-col cols="2"> a </v-col>
-              <v-col cols="10"> b </v-col>
+              <v-col cols="2">
+                <v-radio @click="toggle = !toggle" v-model="toggle"> </v-radio>
+              </v-col>
+              <v-col cols="10">
+                <p class="text-subtitle-1 pt-1">
+                  {{ item.description }}
+                </p>
+              </v-col>
             </v-row>
           </v-sheet>
         </v-card-text>
+        <v-card-actions>
+          <v-row justify="end">
+            <v-btn> adicionar tarefa </v-btn>
+          </v-row>
+        </v-card-actions>
       </v-card>
     </v-row>
   </div>
